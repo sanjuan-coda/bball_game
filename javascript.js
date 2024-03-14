743170bd7478ecd36155ec2d69d55bdc5edf637e
sanjuan-coda <sam.thasing42@gmail.com>
2024-03-14 19:25:40 +0100

    titlescreen
    opmaakt titel scherm

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 console.log("NY Knicks Nation")
+
+let h1element = document.querySelector("h1")
+document.body.onkeyup = function(gadoor) {
+    if (gadoor.key == " " ||
+        gadoor.code == "spatiebalk" ||      
+        gadoor.keyCode == 32      
+    ) {
+        h1element.textContent = "f*ck it! we ball!"
+    }
+  }
