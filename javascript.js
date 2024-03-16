93718f7c8675dd85a98de7309cce49296c054583
sanjuan-coda <sam.thasing42@gmail.com>
2024-03-16 13:40:24 +0100

    trying to make sense
    verbeteren keydown

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
 console.log("NY Knicks Nation")
 
-let h1element = document.querySelector("h1")
-document.body.onkeyup = function(gadoor) {
-    if (gadoor.key == " " ||
-        gadoor.code == "spatiebalk" ||      
-        gadoor.keyCode == 32      
-    ) {
-        h1element.textContent = "f*ck it! we ball!"
+
+// let h1element = document.querySelector("h1")
+// document.body.onkeyup = function(gadoor) {
+//     console.log(gadoor) 
+//     if (gadoor.key == " " &&
+//         gadoor.code == "spatiebalk" &&      
+//         gadoor.keyCode == 33
+//     ) {
+//         h1element.textContent = "f*ck it! we ball!"
+//     }
+//   }
+
+document.querySelector('body').AddEventlistener('keydown' , function(event){
+    if(event.keyCode === 32){
+        console.log(event);
     }
-  }
+});
