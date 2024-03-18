b7738c6d3041b5bb019f5b6e3107f0ac2827b4bd
sanjuan-coda <sam.thasing42@gmail.com>
2024-03-18 22:55:31 +0100

    fuck this bruh

--- javascript.js
+++ javascript.js
@@ -1,19 +1,24 @@
 console.log("NY Knicks Nation")
 
 
-// let h1element = document.querySelector("h1")
-// document.body.onkeyup = function(gadoor) {
-//     console.log(gadoor) 
-//     if (gadoor.key == " " &&
-//         gadoor.code == "spatiebalk" &&      
-//         gadoor.keyCode == 33
-//     ) {
-//         h1element.textContent = "f*ck it! we ball!"
-//     }
-//   }
+let h1element = document.querySelector("h1")
 
-document.querySelector('body').AddEventlistener('keydown' , function(event){
-    if(event.keyCode === 32){
-        console.log(event);
-    }
-});
+document.body.onkeyup = function(gadoor) {
+console.log(gadoor) 
+if (gadoor.code == "spatiebalk" ||   
+    gadoor.keyCode == 32
+    ) {
+    h1element.textContent = "f*ck it! we ball!"
+   }
+ }
+
+if (h1element.textContent = "f*ck it! we ball!"){
+    setTimeout(function, milliseconds)
+}
+
+//document.querySelector('body').AddEventlistener('keydown' , function(event){
+//   if(event.keyCode === 32){
+//           console.log(event)
+//        h1element.textContent = "f*ck it! we ball!"
+//}
+//})
