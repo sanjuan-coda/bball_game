81adb3be109f991c113de74328d8f0057aa5e89b
sanjuan-coda <sam.thasing42@gmail.com>
2024-03-19 14:20:27 +0100

    titlescreen
    fixed it!

--- javascript.js
+++ javascript.js
@@ -1,25 +1,45 @@
-console.log("NY Knicks Nation")
+console.log("NY Knicks Nation");
 
+const basketball_icon = document.querySelector(".basketball_icon");
+const h1element = document.querySelector("h1");
+const spatiebalk = document.querySelector(".spatiebalk");
+const loading = document.querySelector(".loading");
 
-let h1element = document.querySelector("h1")
+basketball_icon.style.visibility = "hidden";
+loading.style.visibility = "hidden";
 
-document.body.onkeyup = function(gadoor) {
-console.log(gadoor) 
-if (gadoor.code == "spatiebalk" ||   
-    gadoor.keyCode == 32
-    ) {
-    h1element.textContent = "f*ck it! we ball! Double click!"
-   }
- }
+document.body.onkeydown = function verder(gadoor) {
+  console.log(gadoor);
+  if (gadoor.code == "spatiebalk" || gadoor.keyCode == 32) {
+    h1element.textContent = "f*ck it! we ball!";
+    laadscherm();
+  }
+};
 
-document.ondblclick = function(laadscherm){
-console.log(laadscherm)
-if (h1element.textContent == "f*ck it! we ball!")
-setInterval((3000) => {
-    
-}, interval);
+function laadscherm() {
+  basketball_icon.style.visibility = "hidden";
+  loading.style.visibility = "hidden";
+  setTimeout(hideanswer, 3000);
 }
 
+function hideanswer() {
+  basketball_icon.style.visibility = "visible";
+  loading.style.visibility = "visible";
+  h1element.textContent = " ";
+  spatiebalk.textContent = " ";
+  setTimeout(showgame, 3000)
+}
+
+function showgame() {
+  basketball_icon.style.visibility = "hidden";
+  loading.style.visibility = "hidden";
+}
+
+//document.ondblclick = function(laadscherm){
+//  console.log(laadscherm)
+//  if (h1element.textContent == "f*ck it! we ball!")
+//  setInterval(code)
+//  }
 //document.querySelector('body').AddEventlistener('keydown' , function(event){
 //   if(event.keyCode === 32){
 //           console.log(event)
